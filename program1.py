ac7ead0354ba75ed244dcb25359ca1c685b690d0
davidprager <84202740+davidprager@users.noreply.github.com>
2023-08-23 15:53:50 -0400

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,7 +1,7 @@
 """
 CSCI-UA.0002 Spring 2024
 Assignment #1, Program #1
-David
+Your name here
 """
 # This program calculates the average age of three members of a band
 # that are stored in variables and prints out the information
