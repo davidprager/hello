fe3191a6f87ceeeeccf7b3a0f1293506b9186a3f
davidprager <david_prager@yahoo.com>
2023-08-27 20:37:08 -0400

    Change program

--- program1.py
+++ program1.py
@@ -1,7 +1,7 @@
 """
 CSCI-UA.0002 Spring 2024
 Assignment #1, Program #1
-Your name here
+Your nam here
 """
 # This program calculates the average age of three members of a band
 # that are stored in variables and prints out the information
