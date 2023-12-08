2402819d3a97c727c2ae948bc3c41698958a1790
davidprager <84202740+davidprager@users.noreply.github.com>
2023-12-07 21:18:47 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -3,7 +3,7 @@ CSCI-UA.0002 Spring 2024
 Test Program
 """
 # This program will make sure that you are able to run a simple program in Pycharm
-# Update the program to include your name instead of Fred
+# Update the program to include your name instead of David
 # and then commit the changes back to Github
 
 
